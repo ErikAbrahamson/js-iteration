81da51cb36c3e40a5dc98d5e8fc56d55ec0e12f4
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-27 12:24:35 -0600

    Refactor for modularity

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,20 @@
-// Prices vars
+// Price vars
 var options = document.getElementsByTagName('select')[0];
 var span = document.getElementById('answer');
 var resultText = null;
 var answer = function() {
+  span.innerHTML = null;
   span.appendChild(resultText);
 };
 
 // Sum All button
 sum_all.addEventListener('click', function() {
-  span.innerHTML = null;
   resultText = document.createTextNode(sum(options));
   answer();
 });
 
 // Count Selected button
 count_selected.addEventListener('click', function() {
-  span.innerHTML = null;
   resultText = document.createTextNode(count(options));
   answer();
 });
