ddad041ee35caab760707e24382af9a606f398a6
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-27 12:44:51 -0600

    Finish prices iterations

--- js/dom.js
+++ js/dom.js
@@ -6,15 +6,28 @@ var answer = function() {
   span.innerHTML = null;
   span.appendChild(resultText);
 };
-
 // Sum All button
 sum_all.addEventListener('click', function() {
   resultText = document.createTextNode(sum(options));
   answer();
 });
-
 // Count Selected button
 count_selected.addEventListener('click', function() {
   resultText = document.createTextNode(count(options));
+  answer();
+});
+// Sum Selected button
+sum_selected.addEventListener('click', function() {
+  resultText = document.createTextNode(sumSelected(options));
+  answer();
+});
+// Avg all button
+average_all.addEventListener('click', function() {
+  resultText = document.createTextNode(average(options));
+  answer();
+});
+// Avg selected button
+average_selected.addEventListener('click', function() {
+  resultText = document.createTextNode(averageSelected(options));
   answer();
 });
--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,4 @@
-/*
-
-Hints for the first problem:
-
-  1. Define a function that:
-    - is assigned to a variable named `sum`
-    - has one parameter named `options`
-    - returns the calculated sum of all of the values of all of the options passed in
-
-NOTE: use a loop to make this happen.
-
-*/
-
+// Sum
 function sum(options) {
   var answer = null;
   for (var i = 0; i < options.length; i++) {
@@ -18,7 +6,23 @@ function sum(options) {
   }
   return answer;
 }
-
+// Count
 function count(options) {
   return options.selectedOptions.length;
+}
+// Sum selected
+function sumSelected(options) {
+  var answer = null;
+  for(var i = 0; i < options.selectedOptions.length; i++) {
+    answer += Number(options.selectedOptions[i].value);
+  }
+  return answer;
+}
+// Avg
+function average(options) {
+  return sum(options) / options.length;
+}
+// Avg selected
+function averageSelected(options) {
+  return sumSelected(options) / options.selectedOptions.length;
 }
